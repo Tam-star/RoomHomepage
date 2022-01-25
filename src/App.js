bd5855b4f00989319505321acf989b7a566f323f
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-25 01:13:27 +0100

    Added links

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
 import "./style/App.scss";
+import React from "react";
 import TopArticle from "./components/top-article";
-import bottomRightImage from "./assets/images/bottom-right-image.jpg"
-import bottomLeftImage from "./assets/images/bottom-left-image.jpg"
+import bottomRightImage from "./assets/images/bottom-right-image.jpg";
+import bottomLeftImage from "./assets/images/bottom-left-image.jpg";
 
 function App() {
+  const [selectedArticle, setSelectedArticle] = React.useState(0);
+
+  const handleNextArticle = () => {
+    if (selectedArticle === 2) {
+      setSelectedArticle(0);
+    } else {
+      setSelectedArticle(selectedArticle + 1);
+    }
+  };
+
+  const handlePreviousArticle = () => {
+    if (selectedArticle === 0) {
+      setSelectedArticle(2);
+    } else {
+      setSelectedArticle(selectedArticle - 1);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>room</h1>
         <nav>
           <ul>
-            <li>home</li>
-            <li>shop</li>
-            <li>about</li>
-            <li>contact</li>
+            <li><a href="#">home</a></li>
+            <li><a href="#">shop</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">contact</a></li>
           </ul>
         </nav>
       </header>
       <main>
-        <TopArticle articleId={1} />
+        <TopArticle
+          articleId={selectedArticle}
+          handleNextArticle={handleNextArticle}
+          handlePreviousArticle={handlePreviousArticle}
+        />
         <article className="bottom-article">
           <img src={bottomLeftImage}></img>
           <section>
             <h2>ABOUT OUR FURNITURE</h2>
             <p>
               Our multifunctional collection blends design and function to suit
-              your individual taste.Make each room unique, or pick a cohesive
+              your individual taste. Make each room unique, or pick a cohesive
               theme that best express your interests and what inspires you. Find
               the furniture pieces you need, from traditional to contemporary
               styles or anything in between. Product specialists are available
--- src/components/top-article.js
+++ src/components/top-article.js
@@ -1,16 +1,30 @@
 import React from "react";
 import listOfArticles from "../data";
+import angleLeft from "../assets/images/icon-angle-left.svg";
+import angleRight from "../assets/images/icon-angle-right.svg";
+import arrow from "../assets/images/icon-arrow.svg";
 
-export default function TopArticle({ articleId }) {
+export default function TopArticle({
+  articleId,
+  handleNextArticle,
+  handlePreviousArticle,
+}) {
   return (
     <article className="top-article">
       <img src={listOfArticles[articleId].image}></img>
       <section>
-        <h2>{listOfArticles[articleId].title}</h2>
-        <p>{listOfArticles[articleId].text}</p>
-        <p>
-          <a>SHOP NOW</a>
-        </p>
+        <div className="top-article__text">
+          <h2>{listOfArticles[articleId].title}</h2>
+          <p>{listOfArticles[articleId].text}</p>
+          <p className="shop-now">
+            <a>SHOP NOW</a>
+            <img className="shop-now__arrow" src={arrow}></img>
+          </p>
+        </div>
+        <div className="changeSlide">
+          <img src={angleLeft} onClick={handlePreviousArticle}></img>
+          <img src={angleRight} onClick={handleNextArticle}></img>
+        </div>
       </section>
     </article>
   );
